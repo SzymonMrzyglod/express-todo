5c889506ac20ff26eca12b2d3c01c260a39ce766
Szymon Mrzyglod <szymon.mr@gmail.com>
2021-10-17 15:42:42 +0200

    change port

--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const {taskRouter} = require('./routes/task');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/task', taskRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+app.listen(PORT);
